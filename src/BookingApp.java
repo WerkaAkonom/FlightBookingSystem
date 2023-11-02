38ec9f88a03e9edf73bca332e8ac8f79d5a42b55
Weronika Akonom <01183199@pw.edu.pl>
2023-11-02 23:28:07 +0100

    Created algorithm to search multi-section flights. Developed following classes: Manager, MultiSectionRout, Passenger, City, Route, Flight, BookingApp.

--- src/BookingApp.java
+++ src/BookingApp.java
@@ -1,9 +1,27 @@
-public class BookingApp {
+
+class BookingApp {
     Manager manager;
-//    public void showAllFlights(){
-//        System.out.println();
-//    }
+    MultiSectionRout route;
     String fromCity;
     String toCity;
+
+    BookingApp() {
+        manager = new Manager();
+        fromCity = "Phoenix";
+        toCity = "New York";
+    }
+
+    public void startApp(){
+        try{
+            route = manager.getRoute(fromCity, toCity);
+        }
+        catch (NullPointerException nullPointerException){
+            System.out.println("Unfortunately there is no rout from " + fromCity + " to " + toCity);
+        }
+
+        route.getTransitCities().forEach(System.out::println);
+        System.out.println(route.toString());
+    }
+
 
 }
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,6 +1,36 @@
-public class Passenger {
+class Passenger {
     int id;
     String name;
     String surname;
+    public Passenger(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Passenger passenger)) return false;
+
+        if (id != passenger.id) return false;
+        if (!name.equals(passenger.name)) return false;
+        return surname.equals(passenger.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + surname.hashCode();
+        return result;
+    }
 }
